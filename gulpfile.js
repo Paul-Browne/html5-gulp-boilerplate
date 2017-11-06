436634f90ade64f2dca6e1666a5ecbac31194682
Paul Browne <paul.browne@frantic.com>
2017-11-06 18:56:28 +0200

    serve dev not dist for dev server

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ gulp.task('server', function(){
 
                     var dev = express();
                     dev.use(compression())
-                    dev.use(serveStatic('./dist', {
+                    dev.use(serveStatic('./dev', {
                         'extensions': ['html'],
                         'maxAge': 3600000
                     }))
